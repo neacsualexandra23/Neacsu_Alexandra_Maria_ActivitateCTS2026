51d333024cd5f777f13f9e2dcf2eddb2d15176c3
neacsualexandra23 <neacsualexandra23@stud.ase.ro>
2026-05-08 21:21:44 +0300

    14_Command  modifcare  ex2 comanda medicamente catre ajutor farmacist

--- 14_Commnad/src/ro/ase/cts/command/exercitiul2/main/Main.java
+++ 14_Commnad/src/ro/ase/cts/command/exercitiul2/main/Main.java
@@ -12,3 +12,12 @@ public class Main {
         farmacist.trimiteComenzi();
     }
 }
+/*
+Deoarece se face o coada foarte mare la casa farmaciei, dirigintele
+acesteia dorește ca farmacistul care preia rețeta și sa trimită comenzi
+ de aducere medicamente către ajutorul de farmacist. Ajutorul va prelua
+ comenzile de aducere medicamente din depozit si la va aduce pe rând pe
+ toate. In timp ce ajutorul de farmacist aduce medicamentele, farmacistul
+ poate sa preia alte rețete. Sa se implementeze modulul de trimitere al
+ comenzilor de către farmacist către ajutorul de farmacist.
+ */
