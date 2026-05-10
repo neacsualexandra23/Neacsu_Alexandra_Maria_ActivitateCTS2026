96a9bb868e36058d292a20d037d8681506bed9e7
neacsualexandra23 <neacsualexandra23@stud.ase.ro>
2026-05-10 18:32:43 +0300

    09_Composite afisare sectiuni si locuri in tribuna

--- 09_Composite/src/ro/ase/cts/composite/exercitiul6/main/Main.java
+++ 09_Composite/src/ro/ase/cts/composite/exercitiul6/main/Main.java
@@ -17,3 +17,12 @@ public class Main {
         sectiune.afiseaza("a fost sters ");
     }
 }
+/*
+Managerul stadionului dorește organizarea locurilor pe care stau
+spectatorii într-o structura arborescenta. Astfel locurile vor fi
+organizate in cadrul aplicației pe secțiuni (Tribuna, Peluza, etc)
+Fiecare secțiune conține subsecțiuni (Tribuna Nord, Tribuna Sud,
+Tribuna Copii, VIP, etc) sau direct locurile din acea secțiune.
+Sa se realizeze modulul care permite reprezentarea arborescenta a
+ locurilor spectatorilor in cadrul aplicației.
+ */
