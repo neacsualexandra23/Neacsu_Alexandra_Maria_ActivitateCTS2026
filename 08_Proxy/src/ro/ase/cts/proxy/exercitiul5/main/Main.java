cd20e6d40bd6912b907b78c656142db0f525328f
neacsualexandra23 <neacsualexandra23@stud.ase.ro>
2026-05-11 21:42:19 +0300

    08_Proxy autobuzul opreste noaptea doar daca are calatori

--- 08_Proxy/src/ro/ase/cts/proxy/exercitiul5/main/Main.java
+++ 08_Proxy/src/ro/ase/cts/proxy/exercitiul5/main/Main.java
@@ -13,3 +13,14 @@ public class Main {
         autobuz.opresteInStatie();
     }
 }
+
+
+/*
+public interface IAutobuz {
+    void opresteInStatie();
+}
+
+Pentru autobuzele de noapte se doreste ca oprirea in statie sa se faca doar daca exista persoane in autobuz.
+In caz contrar autobuzul se retrage la autobaza fara sa mai realizeze opriri.
+Sa se implementeze modul care va permite oprirea in statie a autobuzelor doar daca exista calatori in autobuz.
+ */
