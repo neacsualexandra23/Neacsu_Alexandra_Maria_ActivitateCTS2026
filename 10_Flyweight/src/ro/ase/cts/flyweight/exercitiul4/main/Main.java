fff23d37d61ac29a69141c398b25c45dfa77d9b9
neacsualexandra23 <neacsualexandra23@stud.ase.ro>
2026-05-14 15:56:31 +0300

    10_Flyweight ex4 printare reteta pentru client instotita de Recomandari

--- 10_Flyweight/src/ro/ase/cts/flyweight/exercitiul4/main/Main.java
+++ 10_Flyweight/src/ro/ase/cts/flyweight/exercitiul4/main/Main.java
@@ -14,3 +14,16 @@ public class Main {
     recomnadare.afiseazaReteta();
     }
 }
+/*
+Managementul spitalului dorește implementarea unei modul de printare
+a rețetelor pe care le primesc pacienții alături de diagnosticul lor
+principal cu care ies din spital. Alături de aceste informații,
+pentru a facilita accesul la informații ce vizează sătătatea populației, rețetele printate
+conțin și un set aleatoriu selectat de recomandări de sănătate
+generală, de ex: consumul de minim 2 litri de apă pe zi, numărul de
+ore de somn, evitarea consumului de sare și zahăr, etc.
+Cunoscându-se faptul că există un număr limitat de seturi de
+recomandări generale privind sănătatea în comparație cu numărul mare
+de rețete ce se tipăresc în spital, să se implementeze un modul ce
+optimizează spațiul de memorie ocupat privind listarea rețetelor.
+ */
